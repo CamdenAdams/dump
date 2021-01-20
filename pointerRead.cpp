a63b7eabd279a78d8dd96870fbd44b8aa382cf70
bert <camdenbadams@gmail.com>
2021-01-20 15:21:55 -0800

    pointerRead successfully reads int pointer at ptr2Int variable address. then successfully reads value at address found within ptr2Int

--- pointerRead.cpp
+++ pointerRead.cpp
@@ -1,12 +1,10 @@
-// this program is designed to return the value of ptr2int (address of variable) and the value at that address
-
-// instructions:
-// Reading a pointer
-// You are going to read the value of the pointer "ptr2int" in our dummy program, then you will read the int at the address pointed by this pointer, which will be our integer equal to 123456
-// Important thing to remember when dealing with pointers: x86 pointers are 4 bytes long (32 bits) and x64 pointers are 8 bytes long (64 bits) so adapt the parameter "nSize" or ReadProcessMemory depending on the architecture of your target program.
-
-
-// 
+/**
+ * this program is designed to return the value of ptr2int (address of variable) and the value at that address
+ * 
+ * Reading a pointer
+ * You are going to read the value of the pointer "ptr2int" in our dummy program, then you will read the int at the address pointed by this pointer, which will be our integer equal to 123456
+ * Important thing to remember when dealing with pointers: x86 pointers are 4 bytes long (32 bits) and x64 pointers are 8 bytes long (64 bits) so adapt the parameter "nSize" or ReadProcessMemory depending on the architecture of your target program.
+ */
 
 #include<iostream>
 #include<windows.h>
@@ -20,10 +18,11 @@ int main(int, char*[]) {
     SIZE_T nSize;
 
     
-    // attacker application variables
-    int readInt;
-    uintptr_t ptr2ptrAddress;
+    // actor application variables
     uintptr_t ptr2IntAddress;
+    uintptr_t readIntPtr;
+
+    DWORD readIntValue;
 
     // provided from task manager or dumb application output
     cout << "Provide target processID: " << endl;
@@ -41,24 +40,16 @@ int main(int, char*[]) {
 
     // provided from output of dumb application output
     cout << "Provide memory address of target variable (ptr2int): " << endl;
-    cin >> hex >> ptr2IntAddress;
-
-    // A "buffer" simply means a place in memory.
-    // SIZE_T is the size of an object in bytes
-
-    // Read Process Memory
-        // At given handle (HANDLE hProcess)
-        // Here's the memory address of the variable that I would like to read (LPCVOID lpBaseAddress) - Is this a constant because the memory space has already been allocated?
-        // Here's the memory address in which I want to store the value read at the memory address provided in parameter two (LPVOID lpBuffer) - Is this not a constanct because the memory space has NOT been alocated?
-        // The size, in bytes, of the target value specified at address provided in parameter two
-        // 
-        // DOCUMENTATION: 
-
+    cin >> uppercase >> hex >> ptr2IntAddress;
 
     /**
      * Read Process Memory (https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-readprocessmemory)
      * 
      * Function used to read value from target processID and specified memory location INTO specified memory location within acting process
+     * 
+     * Some neat notes!
+     *  - A "buffer" simply means a place in memory.
+     *  - SIZE_T is the size of an object in bytes
      * 
      * @param hProcess - given handle, initialized with OpenProcess, handle must have PROCESS_VM_READ privilages at a minimum to read successfully
      * @param ptr2IntAddress - the memory address of the variable that is to be read. Cast to LPCVOID to prevent errors since this value is provided through console input, I want to mess with this variable declaration
@@ -68,11 +59,31 @@ int main(int, char*[]) {
      * 
      * @return if successful, returns nonzero, otherwise zero
      */
-    BOOL rpmReturn = ReadProcessMemory(hProcess, (LPCVOID)ptr2IntAddress, &readInt, sizeof(LPCVOID), NULL);
-    if(rpmReturn == FALSE) {
+    BOOL rpm = ReadProcessMemory(hProcess, (LPCVOID)ptr2IntAddress, &readIntPtr, sizeof(LPCVOID), NULL);
+    if(rpm == FALSE) {
         cout << "ReadProcessMemory failed to read value at " << hex << uppercase << ptr2IntAddress << endl;
         getchar();
         return EXIT_FAILURE;
     }
+
+    cout << "readIntPtr = " << hex << uppercase << readIntPtr << endl;
+    cout << "Press any key..." << endl << endl;
+    getchar();
+
+    rpm = ReadProcessMemory(hProcess, (LPCVOID)readIntPtr, &readIntValue, sizeof(int), NULL);
+    if(rpm == FALSE) {
+        cout << "ReadProcessMemory failed to read value at " << hex << uppercase << ptr2IntAddress << endl;
+        getchar();
+        return EXIT_FAILURE;
+    }
+
+    cout << "readIntPtr = " << hex << uppercase << readIntPtr << endl;
+    cout << "readIntAddress = " << dec << readIntValue << endl;
+    cout << "Press any key..." << endl << endl;
+    getchar();
+
+    CloseHandle(hProcess);
+
+    return EXIT_SUCCESS;
 
 }
